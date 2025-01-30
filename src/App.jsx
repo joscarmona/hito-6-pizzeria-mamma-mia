e5315848781e9722fb976afcce47733788c87b2b
Jose Carmona <josshue85@gmail.com>
2025-01-30 01:36:38 -0300

    Context - Provider implementado

--- src/App.jsx
+++ src/App.jsx
@@ -12,32 +12,35 @@ import NotFound from './pages/NotFound'
 
 /* COMPONENTES Routes & Route */
 import { Navigate, Route, Routes } from 'react-router-dom'
+import CartProvider from './context/CartContext'
 
 function App() {
   return (
     <section className="layout">
       {/* SIEMPRE SE MOSTRARAN LOS COMPONENTES NAVBAR & FOOTER */}
-      {/* *** Navbar *** */}
-      <Navbar />      
-      {/* RUTAS */}
-      <Routes>
-        {/* *** Home *** */}
-        <Route path='/' element={<Home />} />
-        {/* *** RegisterPage *** */}
-        <Route path='/register' element={<RegisterPage />} />
-        {/* *** LoginPage *** */}
-        <Route path='/login' element={<LoginPage />} />
-        {/* *** Cart *** */}
-        <Route path='/cart' element={<Cart />} />
-        {/* *** Pizza *** */}
-        <Route path='/pizza/p001' element={<Pizza />} />
-        {/* *** ProfilePage *** */}
-        <Route path='/profile' element={<ProfilePage /> } />
-        {/* *** NotFound *** */}
-        <Route path='/404' element={<NotFound />} />
-        {/* El componente Navigate permite redireccionar una ruta */}
-        <Route path='*' element={<Navigate to="/404" />} />
-      </Routes>    
+      <CartProvider>
+        {/* *** Navbar *** */}
+        <Navbar />
+        {/* RUTAS */}
+        <Routes>
+          {/* *** Home *** */}
+          <Route path='/' element={<Home />} />
+          {/* *** RegisterPage *** */}
+          <Route path='/register' element={<RegisterPage />} />
+          {/* *** LoginPage *** */}
+          <Route path='/login' element={<LoginPage />} />
+          {/* *** Cart *** */}
+          <Route path='/cart' element={<Cart />} />
+          {/* *** Pizza *** */}
+          <Route path='/pizza/p001' element={<Pizza />} />
+          {/* *** ProfilePage *** */}
+          <Route path='/profile' element={<ProfilePage /> } />
+          {/* *** NotFound *** */}
+          <Route path='/404' element={<NotFound />} />
+          {/* El componente Navigate permite redireccionar una ruta */}
+          <Route path='*' element={<Navigate to="/404" />} />
+        </Routes>
+      </CartProvider>
       {/* *** Footer *** */}
       <Footer />      
     </section>
